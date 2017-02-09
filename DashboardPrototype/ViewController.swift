9af1bc610e98896b08443e59e8da577cc82fe5dc
Nathanael Gethers <gethernm@gmail.com>
2017-02-09 16:40:03 -0600

    updated bottom content

--- DashboardPrototype/ViewController.swift
+++ DashboardPrototype/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, iCarouselDelegate, iCarouselDataSource{
     @IBOutlet weak var descriptionLabel: SpringLabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var carousel: iCarousel!
+    @IBOutlet weak var appointmentViewBox: UIView!
+    @IBOutlet weak var messageViewBox: UIView!
     var lastIndex = 0
     var data: [[Int:String]] = [
         [1 : "one"],
@@ -25,7 +27,9 @@ class ViewController: UIViewController, iCarouselDelegate, iCarouselDataSource{
         [6 : "six"],
         [7 : "seven"],
         [8 : "eight"],
-        [9 : "nine"]
+        [9 : "nine"],
+        [10 : "ten"],
+        [11 : "eleven"],
     ]
     let tilesDescriptions = [
         "The Health app shows your health and fitness information all in one place. To use the Health app, download the latest version of iOS for your iPhone or iPod touch. Then you can track health information from multiple sources, like compatible apps and fitness accessories. Set up health data. Apple Watch data.",
@@ -35,14 +39,13 @@ class ViewController: UIViewController, iCarouselDelegate, iCarouselDataSource{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        appointmentViewBox.createBox()
+        messageViewBox.createBox()
+        
         carousel.type = .coverFlow
         carousel.scrollToItem(at: data.count/2, animated: false)
         carousel.isPagingEnabled = true
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     func numberOfItems(in carousel: iCarousel) -> Int {
@@ -52,29 +55,24 @@ class ViewController: UIViewController, iCarouselDelegate, iCarouselDataSource{
     
     func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
         let imageView: UIView
-        let label = UILabel(frame: CGRect(x: 0, y: 150, width: 200, height: 50))
+//        let backgroundImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
         let backgroundImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
         
         if view != nil {
             imageView = view!
         } else {
             imageView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-            imageView.backgroundColor = UIColor(red: 69/255.0, green: 195/255.0, blue: 171/255.0, alpha: 1.0)
-            
             backgroundImage.image = UIImage(named: getImageName(atIndex: index))
-            label.text = getSelectDataText(index)
-            label.textColor = UIColor.white
-            label.textAlignment = .center
-            label.font = UIFont(name: label.font.fontName, size: 36)
+//            imageView.backgroundColor = randomColor()
+//            backgroundImage.contentMode = UIViewContentMode.scaleAspectFit
+            backgroundImage.center = imageView.center
         }
         
         imageView.addSubview(backgroundImage)
-        //imageView.addSubview(label)
         
         return imageView
     }
-
-    
+   
     func carouselDidEndScrollingAnimation(_ carousel: iCarousel) {
         let currentViewIndex = carousel.currentItemIndex
         descriptionLabel.text = tilesDescriptions[currentViewIndex%2]
@@ -83,18 +81,20 @@ class ViewController: UIViewController, iCarouselDelegate, iCarouselDataSource{
         titleLabel.text = getSelectedIndexText(indexValue: currentViewIndex)
     }
     
+    // Animating the text
     func transitionLabel(_ currentIndex: Int) {
         if lastIndex == currentIndex {
             return
         }
         else if lastIndex > currentIndex {
-            descriptionLabel.animation = "slideRight"
+            descriptionLabel.animation = "fadeInRight"
         }else {
-            descriptionLabel.animation = "slideLeft"
+            descriptionLabel.animation = "fadeInLeft"
         }
+        
         descriptionLabel.curve="easeIn"
         descriptionLabel.autostart = true
-        descriptionLabel.duration = 1.5
+        descriptionLabel.duration = 0.7
         descriptionLabel.animate()
     }
     
@@ -118,9 +118,17 @@ class ViewController: UIViewController, iCarouselDelegate, iCarouselDataSource{
 }
 
 func getSelectedIndexText(indexValue index: Int) -> String {
-    return ["Friends & Family", "Fitness", "Points", "Nutrition", "Trackers", "Health Assessment", "", "", ""][index]
+    return ["Health Assessment", "Sync Device", "Friends & Family", "Incentives", "Coach", "Goals", "Challenges", "Tutorials", "Fitness", "Points", "Nutrition", "Trackers"][index]
 }
 
 func getImageName(atIndex index: Int) -> String {
-    return ["familytogether", "fitness", "points", "nutrition", "trackers", "assessment", "familytogether", "fitness", "trackers"][index]
+    return ["assessment", "trackers", "familytogether", "fitness", "nutrition", "nutrition", "fitness", "assessment", "points", "nutrition", "trackers", "trackers"][index]
+}
+
+extension UIView {
+    func createBox(){        
+        self.backgroundColor = UIColor.white
+//        self.layer.borderColor = UIColor.gray.cgColor
+//        self.layer.borderWidth = 1
+    }
 }
